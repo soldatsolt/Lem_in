7a001a6855d07695073544bb1ff98f16513d8f6c
Kiana Mills <kmills@oa-l4.21-school.ru>
2019-09-09 00:52:44 +0300

    Нарисовал эту табличку

--- lem_in.h
+++ lem_in.h
@@ -4,10 +4,17 @@
 # define INT_MAX (2147483647)
 # define INT_MIN (-2147483648)
 
+typedef	struct	s_table
+{
+	int			k;
+	char		*str;
+}				t_table;
+
 typedef	struct	s_graph
 {
-	int	size;
-	int	**matrix;
+	int			size;
+	int			**matrix;
+	t_table		*table;
 }				t_graph;
 
 #endif
--- main.c
+++ main.c
@@ -8,6 +8,7 @@ void	allocate_graph(t_graph *graph)
 	j = 0;
 	i = 0;
 	graph->matrix = (int **)x_malloc(sizeof(int *) * graph->size);
+	graph->table = (t_table *)x_malloc(sizeof(t_table) * graph->size);
 	while (i < graph->size)
 	{
 		graph->matrix[i] = (int *)x_malloc(sizeof(int) * graph->size);
@@ -32,6 +33,7 @@ void	free_graph(t_graph *graph)
 		i++;
 	}
 	free(graph->matrix);
+	free(graph->table);
 	free(graph);
 }
 
@@ -42,8 +44,17 @@ void	print_matr_graph(t_graph *graph)
 
 	j = 0;
 	i = 0;
+	ft_putstr("       |");
 	while (i < graph->size)
 	{
+		ft_printf("%7s|", graph->table[i].str);
+		i++;
+	}
+	ft_putchar('\n');
+	i = 0;
+	while (i < graph->size)
+	{
+		ft_printf("%7s|", graph->table[i].str);
 		while (j < graph->size)
 		{
 			ft_printf("___%d___|", graph->matrix[i][j]);
@@ -55,24 +66,55 @@ void	print_matr_graph(t_graph *graph)
 	}
 }
 
+char	*first_word_of_str(char *str)
+{
+	char	*s;
+	int		i;
+
+	s = ft_strdup(str);
+	while (s[i] && s[i] != ' ' && s[i] != '\t')
+		i++;
+	s[i] = '\0';
+	return (s);
+}
+
+void	put_str_to_table(t_graph *graph, char *str, int i)
+{
+	char	*s;
+
+	s = first_word_of_str(str);
+	graph->table[i].str = ft_strdup(s);
+	graph->table[i].k = i;
+	free(s);
+}
+
 int		main(void)
 {
 	t_graph	*graph;
 	char	*str;
+	int		i;
+	int		o;
 
+	i = 0;
 	graph = (t_graph *)x_malloc(sizeof(t_graph));
-	open("map", O_RDONLY);
+	o = open("map", O_RDONLY);
 	get_next_line(3, &str);
 	graph->size = ft_atoi(str);
 	free(str);
 	allocate_graph(graph);
 	while (get_next_line(3, &str))
 	{
+		if (str[0] != '#' && i < graph->size)
+		{
+			put_str_to_table(graph, str, i);
+			i++;
+		}
 		ft_printf("%s\n", str);
 		ft_strdel(&str);
 	}
-	close(3);
+	close(o);
 	print_matr_graph(graph);
 	free_graph(graph);
+
 	return (0);
 }
